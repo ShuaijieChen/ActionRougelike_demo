c421dd75dc4b77b25ed4c9f1eb6e18c8977a3034
ShuaijieChen <2430727258@qq.com>
2023-06-01 16:02:51 +0800

    create a character
    add a character that allow move forward and turn around

---
+++ Source/Roguelike/Private/MyCharacter.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");//创建弹簧臂并命名
+	SpringArmComp->SetupAttachment(RootComponent);//将弹簧臂绑定在character上
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");//创建摄像机并命名
+	CameraComp->SetupAttachment(SpringArmComp);//将摄像机绑定在弹簧臂上
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMyCharacter::MoveForward(float value)
+{
+	AddMovementInput(GetActorForwardVector(), value);
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMyCharacter::MoveForward);
+
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+}
+
